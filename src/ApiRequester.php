e8ae5d61ea490ae2c7b79c9eedf829023de9f945
Vinorcola <vinorcola@gmail.com>
2018-03-19 11:09:25 +0100

    Possibility to set headers in ApiRequester.

--- src/ApiRequester.php
+++ src/ApiRequester.php
@@ -10,12 +10,16 @@ class ApiRequester
      * @param string     $method
      * @param string     $url
      * @param mixed|null $body
+     * @param array      $headers
      * @return mixed
      */
-    public static function request(string $method, string $url, $body = null)
+    public static function request(string $method, string $url, $body = null, array $headers = [])
     {
         $connection = curl_init($url);
         curl_setopt($connection, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($connection, CURLOPT_HTTPHEADER, array_map(function ($key, $value) {
+            return $key . ': ' . $value;
+        }, array_keys($headers), $headers));
         curl_setopt($connection, CURLOPT_RETURNTRANSFER, true);
         if ($body) {
             curl_setopt($connection, CURLOPT_POSTFIELDS, $body);
